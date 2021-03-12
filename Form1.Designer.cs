25a95ce29016af08a7dc5dbe3b7663ac7672eef2
EndiFox <76921901+EndiFox@users.noreply.github.com>
2021-03-12 18:47:46 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿
+namespace kaskk
+{
+    partial class QB
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+            int w = 1280;
+            int h = 720;
+            base.Width = w;
+            base.Height = h;
+        }
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QB));
+            this.SuspendLayout();
+            // 
+            // QB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuText;
+            this.ClientSize = new System.Drawing.Size(1264, 682);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "QB";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quantum-Borderworlds";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows;
+
+
+namespace kaskk
+{
+    static class BQWorld
+    {
+
+
+        public class Item
+        {
+            float x = 0.0f;
+            float y = 0.0f;
+            float z = 0.0f;
+            int id = 0;
+            int k = 0;
+            string name = null;
+        }
+        
+        public class Tree
+        {
+            float x = 0.0f;
+            float y = 0.0f;
+            float z = 0.0f;
+            bool cutted = false;
+            int stage = 0;
+            int intreetimer = 0;
+        }
+        public class Axe
+        {
+            float x = 0.0f;
+            float y = 0.0f;
+            float z = 0.0f;
+            bool aleb = false;
+            bool standart = false;
+            int usage = 0;
+            int damage = 0;
+            int manahave = 0;
+            int id = 0;
+            int level = 0;
+        }
+        
+        public class Player
+        {
+            float x = 0.0f;
+            float y = 0.0f;
+            float z = 0.0f;
+            float fov = 3.14159f / 3;
+            float a = 0.0f;
+            float depth = 30.0f;
+            float health = 100.0f;
+            int armor = 0;
+            int hunger = 200;
+            float mana = 10;
+            int[] inventory = new int[10];
+            string[] names = new string[10];
+            int[] ids = new int[10];
+            //float plaIntersect(in vec3 ro, in vec3 rd, in vec4 p)
+            //{
+            //    return -(dot(ro, p.xyz) + p.w) / dot(rd, p.xyz);
+            //}
+        }
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new QB());
+            
+            static void dot(float ro, float rd)
+            {
+                
+            }
+            /// Basic ints and arrays
+            Random rnd = new Random();
+            int w = 1280;
+            int h = 720;
+            float maxX = 40.0f;
+            float maxY = 40.0f;
+            float minX = -40.0f;
+            float minY = -40.0f;
+            float maxZ = 20.0f;
+            float minZ = -20.0f;
+            float[,,] GlItemPos = new float[20000, 20000, 20000];
+            int[,] b = new int[39,39];
+            int[,] stones_Mass = new int[40, 40];
+
+            /// Basic ints and arrays
+
+            /// Tree generator 
+            for (int i = 0; i != 78; i++ )
+            {
+                if (i == 78)
+                {
+                    break;
+                }
+                
+                int rndtrees = rnd.Next(-39, 39);
+                b[i, i] = rndtrees;
+
+            }
+            /// Tree generator 
+
+            ///Stone generator
+            for (int i = 0; i != 80; i++)
+            {
+                if (i == 78)
+                {
+                    break;
+                }
+
+                int rndstones = rnd.Next(-40, 40);
+                stones_Mass[i, i] = rndstones;
+
+            }
+            ///Stone generator
+
+            ///Generator examples
+            Player player1 = new Player();
+            ///Generator examples
+
+            /// Main program         
+            while (true) 
+            {
+
+            }
+        }
+    }
+}
